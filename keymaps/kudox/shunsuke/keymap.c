6b04111e4943f788910d5608cd4103b636b58d37
hidashun <hidashun@users.noreply.github.com>
2020-01-20 08:33:37 +0900

    Update kudox 2nd

--- keymaps/kudox/shunsuke/keymap.c
+++ keymaps/kudox/shunsuke/keymap.c
@@ -18,20 +18,22 @@
 #define KC_L1SYM LT(_SYMB, KC_LANG1)
 #define KC_L2SYM LT(_SYMB, KC_LANG2)
 #define KC_LAENT LT(_SYMB, KC_ENT)
- 
+#define KC_LASPC LT(_SYMB, KC_SPC)
+#define KC_LAESC LT(_SYMB, KC_ESC)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┬────────┐                ┌────────┬────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_ESC  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,KC_6    ,                 SYM_L    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_MINS ,KC_BSPC ,
+     KC_ESC  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,KC_6    ,                 KC_GRV  ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_MINS ,KC_BSPC ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,KC_GRV  ,                 KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_LBRC ,KC_RBRC ,
+     KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,KC_LAESC,                 KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_LBRC ,KC_RBRC ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┘                └────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LCTL ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,                                   KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_QUOT ,KC_ENT  ,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                                  ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,                                   KC_BSPC ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_BSLS ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                ┌────────┼────────┼────────┴────────┼────────┼────────┼────────┤
-     SYM_L   ,KC_MINS ,KC_LALT ,KC_LGUI ,KC_SPC  ,KC_ALSP ,KC_SLSH ,                 KC_SCLN ,KC_EQL  ,KC_LAENT,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT
+     SYM_L   ,KC_MINS ,KC_LALT ,KC_LGUI ,KC_LASPC,KC_ALSP ,KC_SLSH ,                KC_SCLN ,KC_EQL  ,KC_LAENT,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT
   //└────────┴────────┴────────┴────────┴────────┴────────┴────────┘                └────────┴────────┴────────┴────────┴────────┴────────┴────────┘
   ),
 
@@ -45,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                                  ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______ ,XXXXXXX ,XXXXXXX ,XXXXXXX ,KC_DOT  ,KC_SLSH ,                                   KC_DEL  ,KC_LCBR ,KC_RCBR ,KC_DOT  ,KC_SLSH ,_______ ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                ┌────────┼────────┼────────┴────────┼────────┼────────┼────────┤
-     _______ ,_______ ,_______ ,_______ ,_______ ,KC_LT   ,KC_UNDS ,                 KC_CIRC ,KC_GT   ,_______ ,KC_END  ,KC_PGDN ,KC_PGUP ,KC_HOME
+     _______ ,_______ ,_______ ,_______ ,_______ ,KC_EXLM ,KC_UNDS ,                 KC_DOT  ,KC_MINS ,_______ ,KC_END  ,KC_PGDN ,KC_PGUP ,KC_HOME
   //└────────┴────────┴────────┴────────┴────────┴────────┴────────┘                └────────┴────────┴────────┴────────┴────────┴────────┴────────┘
   )
 };
