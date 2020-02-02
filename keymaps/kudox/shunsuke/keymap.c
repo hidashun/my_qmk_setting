779924810c9ef4bc60a4d100001a169c95fbe93b
hidashun <hidashun@users.noreply.github.com>
2020-02-02 11:31:40 +0900

    Add outerworld

--- keymaps/kudox/shunsuke/keymap.c
+++ keymaps/kudox/shunsuke/keymap.c
@@ -6,12 +6,16 @@
 // entirely and just use numbers.
 #define _QWERTY 0
 #define _SYMB 1
+#define _BORDERLANDS3 2
+#define _OUTERWORLD 3
 
 // Shortcut to make keymap more readable
 #define SYM_L   MO(_SYMB)
 
 #define KC_ALES LALT_T(KC_ESC)
 #define KC_ALSP LALT_T(KC_SPC)
+#define KC_AL_K LALT_T(KC_K)
+#define KC_AL_J LALT_T(KC_J)
 
 #define KC_RGENT MT(KC_RGUI, KC_ENT)
 #define KC_LGSPC GUI_T(KC_SPC)
@@ -31,6 +35,9 @@
 #define KC_SPELR LGUI(LCTL(LSFT(KC_RGHT)))
 #define KC_SPENT LCTL(LALT(KC_RGHT))
 #define KC_SPEPT LCTL(LALT(KC_LEFT))
+
+#define KC_TGGM1 TG(_BORDERLANDS3)
+#define KC_TGGM2 TG(_OUTERWORLD)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -56,9 +63,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┘                └────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      _______ ,KC_LEFT ,KC_DOWN ,KC_RGHT ,KC_AT   ,KC_HASH ,                                   KC_SPEL ,KC_SPEC ,KC_SPER ,KC_LANG2,KC_LANG1,_______ ,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                                  ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______ ,KC_0    ,KC_CIRC ,KC_DLR  ,KC_ASTR ,KC_PERC ,                                   KC_DEL  ,KC_SPELL,KC_SPELR,KC_LPRN ,KC_RPRN ,_______ ,
+     _______ ,KC_0    ,KC_CIRC ,KC_DLR  ,KC_ASTR ,KC_PERC ,                                   KC_DEL  ,KC_SPELL,KC_SPELR,KC_LPRN ,KC_RPRN ,KC_TGGM2,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                ┌────────┼────────┼────────┴────────┼────────┼────────┼────────┤
-     _______ ,KC_EXLM ,KC_AMPR ,KC_PIPE ,_______ ,KC_SPC  ,KC_ENT  ,                 KC_UP   ,KC_DOWN ,_______ ,KC_HOME ,KC_PGUP ,KC_PGDN ,KC_END
+     _______ ,KC_EXLM ,KC_AMPR ,KC_PIPE ,_______ ,KC_SPC  ,KC_ENT  ,                 KC_UP   ,KC_DOWN ,KC_TGGM1,KC_HOME ,KC_PGUP ,KC_PGDN ,KC_END
+  //└────────┴────────┴────────┴────────┴────────┴────────┴────────┘                └────────┴────────┴────────┴────────┴────────┴────────┴────────┘
+  ),
+  
+  [_BORDERLANDS3] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┬────────┐                ┌────────┬────────┬────────┬────────┬────────┬────────┬────────┐
+     KC_ESC  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,KC_P    ,                 KC_GRV  ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_MINS ,KC_BSPC ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_I    ,KC_L    ,                 KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_LBRC ,KC_RBRC ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┘                └────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_LCTL ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,                                   KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_QUOT ,KC_ENT  ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                                  ├────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_M    ,                                   KC_BSPC ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_BSLS ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                ┌────────┼────────┼────────┴────────┼────────┼────────┼────────┤
+     SYM_L   ,KC_GRV  ,KC_AL_J ,KC_AL_K ,KC_SPC  ,KC_LBRC ,KC_RBRC ,                 KC_SCLN ,KC_EQL  ,KC_TGGM1,KC_LEFT ,KC_UP,   KC_DOWN ,KC_RGHT
+  //└────────┴────────┴────────┴────────┴────────┴────────┴────────┘                └────────┴────────┴────────┴────────┴────────┴────────┴────────┘
+  ),
+
+  [_OUTERWORLD] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┬────────┐                ┌────────┬────────┬────────┬────────┬────────┬────────┬────────┐
+     KC_ESC  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,KC_6    ,                 KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_MINS ,KC_BSPC ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,KC_I    ,                 KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_SLSH ,KC_BSLS ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┘                └────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_LCTL ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,                                   KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_ENT  ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                                  ├────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_M    ,                                   _______ ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_TGGM2,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                ┌────────┼────────┼────────┴────────┼────────┼────────┼────────┤
+     KC_ALES ,KC_COMM ,KC_AL_J ,KC_AL_K ,KC_SPC  ,KC_ENT  ,KC_LGUI  ,                KC_RGENT,KC_DEL  ,_______ ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT
   //└────────┴────────┴────────┴────────┴────────┴────────┴────────┘                └────────┴────────┴────────┴────────┴────────┴────────┴────────┘
   )
+
 };
